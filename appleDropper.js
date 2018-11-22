aa13134bd1e431876f59dd3a6e7c7247be0efa62
Charles Brandt <luftbuefel@users.noreply.github.com>
2018-11-21 16:48:33 -0800

    updated random to work with the new version of microbit JS

--- appleDropper.js
+++ appleDropper.js
@@ -3,22 +3,22 @@ let appleMoveTime = 0
 let apple: game.LedSprite = null
 let lastCheckedTime = 0
 let player: game.LedSprite = null
-function resetApple()  {
-    apple.set(LedSpriteProperty.X, Math.random(5))
+function resetApple() {
+    apple.set(LedSpriteProperty.X, Math.randomRange(0, 4))
     apple.set(LedSpriteProperty.Y, 0)
 }
-input.onButtonPressed(Button.B, () => {
+input.onButtonPressed(Button.B, function () {
     player.change(LedSpriteProperty.X, 1)
 })
-input.onButtonPressed(Button.A, () => {
+input.onButtonPressed(Button.A, function () {
     player.change(LedSpriteProperty.X, -1)
 })
 player = game.createSprite(2, 4)
 lastCheckedTime = input.runningTime()
 appleMoveTime = 800
-apple = game.createSprite(Math.random(5), 0)
+apple = game.createSprite(Math.randomRange(0, 4), 0)
 minimumAppleMoveTime = 300
-basic.forever(() => {
+basic.forever(function () {
     if (input.runningTime() - lastCheckedTime >= appleMoveTime) {
         apple.change(LedSpriteProperty.Y, 1)
         lastCheckedTime = input.runningTime()
